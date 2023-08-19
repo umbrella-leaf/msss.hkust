8931a2b9e6e0af53a93bb36fdd332d00b0968a48
sonichen <1606673007@qq.com>
2023-08-19 17:37:37 +0800

    update logo pic

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,7 +12,7 @@ module.exports = {
   base:'/msss.hkust/',
   // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+    ['link', { rel: 'icon', href: '/img/logo.jpg' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: 'HKUST,香港科技大学,MSSS,内地生' }],
     ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
   ],
@@ -56,7 +56,7 @@ module.exports = {
 
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/logo.png', // 导航栏logo
+    logo: '/img/logo.jpg', // 导航栏logo
     // repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
